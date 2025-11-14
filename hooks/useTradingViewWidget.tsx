46b662ad68df13be3dc5318c579aa42024e40a58
Adewara Damilare <adewaradan@gmail.com>
2025-11-14 02:44:18 +0100

    Implement Auth UI & Layout

--- hooks/useTradingViewWidget.tsx
+++ hooks/useTradingViewWidget.tsx
@@ -9,10 +9,10 @@ const useTradingViewWidget = (
   const containerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    if (!containerRef.current) return;
-    if (containerRef.current.dataset.loaded) return;
+    const container = containerRef.current;
+    if (!container || container.dataset.loaded) return;
 
-    containerRef.current.innerHTML = `
+    container.innerHTML = `
       <div class="tradingview-widget-container__widget" style="width: 100%; height: ${height}px;"></div>
     `;
 
@@ -21,13 +21,13 @@ const useTradingViewWidget = (
     script.async = true;
     script.innerHTML = JSON.stringify(config);
 
-    containerRef.current.appendChild(script);
-    containerRef.current.dataset.loaded = "true";
+    container.appendChild(script);
+    container.dataset.loaded = "true";
 
     return () => {
-      if (containerRef.current) {
-        containerRef.current.innerHTML = ``;
-        delete containerRef.current.dataset.loaded;
+      if (container) {
+        container.innerHTML = ``;
+        delete container.dataset.loaded;
       }
     };
   }, [scriptUrl, config, height]);
