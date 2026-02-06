89c898ef8eeb23e3c97c4824f2698f407a470139
Adewara Damilare <adewaradan@gmail.com>
2026-02-06 21:40:28 +0100

    Implement Search Stocks

---
+++ components/search/SearchCommand.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { Loader2, Star, TrendingUp } from "lucide-react";
+import {
+  CommandDialog,
+  CommandInput,
+  CommandList,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+} from "../ui/command";
+import { Button } from "../ui/button";
+import { searchStocks } from "@/lib/actions/finnhub.actions";
+import { useDebounce } from "@/hooks/useDebounce";
+
+interface SearchCommandProps {
+  renderAs?: "button" | "text";
+  label?: string;
+  initialStocks: StockWithWatchlistStatus[];
+}
+
+export const SearchCommand = ({ renderAs = "button", label = "Add stock", initialStocks }: SearchCommandProps) => {
+  const [open, setOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [stocks, setStocks] = useState<StockWithWatchlistStatus[]>(initialStocks);
+
+  const isSearchMode = !!searchTerm.trim();
+  const displayStocks = isSearchMode ? stocks : stocks?.slice(0, 10);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key.toLowerCase() === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((currentOpen) => !currentOpen);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  const handleSearch = async () => {
+    if (!isSearchMode) return setStocks(initialStocks);
+
+    setLoading(true)
+    try {
+      const results = await searchStocks(searchTerm.trim());
+      setStocks(results);
+    } catch {
+      setStocks([])
+    } finally {
+      setLoading(false)
+    };
+  }
+
+  const debouncedSearch = useDebounce(handleSearch, 300)
+
+  useEffect(() => {
+    debouncedSearch();
+  }, [searchTerm])
+
+  const handleSelectStock = () => {
+    setOpen(false);
+    setSearchTerm('');
+    setStocks(initialStocks);
+  };
+
+  return (
+    <>
+      {renderAs ===  'text' ? (
+        <span onClick={()=> setOpen(true)} className="search-text">
+          {label}
+        </span>
+      ) : (
+        <Button onClick={()=> setOpen(true)} className="search-btn">
+          {label}
+        </Button>
+      )}
+
+    <CommandDialog open={open} onOpenChange={setOpen} className="search-dialog">
+      <div className="search-field">
+        <CommandInput
+          placeholder="Search for stocks..."
+          value={searchTerm}
+          onValueChange={setSearchTerm}
+          className="search-input"
+        />
+        {loading && <Loader2 className="search-loader" />}
+      </div>
+
+      <CommandList className="search-list">
+        {loading ? (
+        <CommandEmpty className="search-list-empty">Loading stocks...</CommandEmpty>
+        ) : displayStocks?.length === 0 ? (
+          <CommandEmpty className="search-list-empty">
+            {isSearchMode ? 'No results found' : 'No stocks available'}
+          </CommandEmpty>
+        ) : (
+          <CommandGroup heading={`${isSearchMode ? 'Search results' : 'Popular stocks'} (${displayStocks?.length || 0})`}>
+            {displayStocks?.map((stock) => (
+              <CommandItem
+                key={stock.symbol}
+                value={stock.symbol}
+                onSelect={() => {
+                  handleSelectStock();
+                  console.log(`Navigating to /stocks/${stock.symbol}`);
+                }}
+                className="search-item"
+              >
+                <Link
+                  href={`/stocks/${stock.symbol}`}
+                  className="search-item-link flex items-center w-full"
+                >
+                  <TrendingUp className="h-4 w-4 text-gray-500 mr-2" />
+                  <div className="flex-1">
+                    <div className="search-item-name">
+                      {stock.name} ({stock.symbol})
+                    </div>
+                    <div className="text-sm text-gray-500">{stock.exchange} | {stock.type}</div>
+                  </div>
+                </Link>
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        )}
+      </CommandList>
+    </CommandDialog>
+    </>
+  );
+};
